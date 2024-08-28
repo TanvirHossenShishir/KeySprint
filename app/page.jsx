942ab622d1631a0734b03c73f36f43c24b416d0f
TanvirHossenShishir <tanvirhossen@iut-dhaka.edu>
2024-08-28 22:00:56 +0600

    implement the transition between waiting room and playground

--- app/page.jsx
+++ app/page.jsx
@@ -6,17 +6,14 @@ import { CgGym } from "react-icons/cg";
 const Home = () => {
 
   var getRoomID = () => {
-    var id = "dashboard";
+    var id = "1";
     return id;
   }
 
   return (
     <div className="bg_dark h-screen px-28 py-5">
       <div className="flex text-center text-white gap-5 h-64">
-        <Link
-          href="/room/9"
-          className="flex-1 rounded-3xl pt-10 bg_light"
-        >
+        <Link href="/room/9" className="flex-1 rounded-3xl pt-10 bg_light">
           <div className="flex justify-center text-6xl pb-3 cl_orange">
             <CgGym />
           </div>
@@ -30,7 +27,10 @@ const Home = () => {
           <p className="text-4xl font-medium">Quick Play</p>
           <p className="text-lg font-normal m-3 cl_gray">Play against others</p>
         </Link>
-        <Link href="/room/1" className="flex-1 rounded-3xl pt-10 bg_light">
+        <a
+          href={`/room/${getRoomID()}`}
+          className="flex-1 rounded-3xl pt-10 bg_light"
+        >
           <div className="flex justify-center text-6xl pb-3 cl_pink">
             <TiGroupOutline />
           </div>
@@ -38,7 +38,7 @@ const Home = () => {
           <p className="text-lg font-normal m-3 cl_gray">
             Play against friends
           </p>
-        </Link>
+        </a>
       </div>
       <div className="rounded-3xl mt-5 p-8 bg_light">
         <p className="text-4xl font-medium text-white pb-3">Leaderboard</p>
---
+++ components/room.jsx
@@ -0,0 +1,231 @@
+"use client";
+
+let socket;
+import React, { useEffect, useState, useRef } from "react";
+import io from "socket.io-client";
+import { words } from "@/lib/words.json";
+
+const Room = ({ roomID }) => {
+  const [username, setUsername] = useState("");
+  const [room, setRoom] = useState("");
+  const socketInitialized = useRef(false);
+  const [roomJoined, setRoomJoined] = useState(false);
+
+  useEffect(() => {
+    if (!socketInitialized.current) {
+      socketInitializer().then(() => {
+        if (roomID) {
+          setRoom(roomID);
+        }
+      });
+      socketInitialized.current = true;
+    }
+
+    return () => {
+      if (socket) {
+        socket.disconnect();
+      }
+    };
+  }, []);
+
+  async function socketInitializer() {
+    if (socket) return;
+
+    await fetch("/api/socket/ioserver");
+    socket = io();
+
+    socket.on("connect", () => {
+      console.log("Socket connected:", socket.id);
+    });
+
+    socket.on("receive-message", (message) => {
+      console.log("Received message:", message);
+
+      const extractedCaretPositions = Object.values(message);
+      console.log("Extracted:", extractedCaretPositions);
+
+      setOtherCaretPositions(extractedCaretPositions);
+    });
+  }
+
+  useEffect(() => {
+    if (room && !roomJoined && socket) {
+      handleJoinRoom();
+    }
+  }, [room, socket]);
+
+  function handleJoinRoom() {
+    if (room && !roomJoined) {
+      console.log("Joining room:", room);
+      socket.emit("join-room", room, () => {
+        console.log("Room joined successfully");
+        setRoomJoined(true);
+      });
+    }
+  }
+
+  const [connected, setConnected] = useState(false);
+  const [userInput, setUserInput] = useState("");
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const [matchingChars, setMatchingChars] = useState(0);
+  const [matchingWords, setMatchingWords] = useState(0);
+  const [isCharRight, setIsCharRight] = useState(true);
+  const currentWord = words[currentIndex];
+  const [currentCaretPosition, setCurrentCaretPosition] = useState([0, 0]);
+  const [otherCaretPositions, setOtherCaretPositions] = useState([
+    [0, 0],
+    [0, 0],
+  ]);
+
+  useEffect(() => {
+    const adjectives = ["Happy", "Funny", "Sunny", "Silly", "Clever", "Brave"];
+    const nouns = ["Cat", "Dog", "Tiger", "Lion", "Elephant", "Kangaroo"];
+
+    const randomAdjective =
+      adjectives[Math.floor(Math.random() * adjectives.length)];
+    const randomNoun = nouns[Math.floor(Math.random() * nouns.length)];
+    const randomUsername = `${randomAdjective}${randomNoun}`;
+    setUsername(randomUsername);
+  }, []);
+
+  const sendMessage = () => {
+    const Message = {
+      userId: username,
+      message: [currentIndex, matchingChars],
+    };
+
+    if (socket) {
+      socket.emit("send-message", {
+        Message,
+        room,
+      });
+    }
+  };
+
+  const handleKeyDown = (event) => {
+    if (event.key === "Backspace") {
+      event.preventDefault();
+
+      setMatchingChars((prevMatchingChars) =>
+        Math.max(0, prevMatchingChars - 1)
+      );
+
+      setUserInput((prevUserInput) => prevUserInput.slice(0, -1));
+
+      setIsCharRight(true);
+    }
+    sendMessage();
+  };
+
+  const handleChange = (event) => {
+    const value = event.target.value;
+
+    if (value[matchingChars] === currentWord[matchingChars]) {
+      setMatchingChars((prevMatchingChars) => prevMatchingChars + 1);
+      setUserInput(value);
+      setIsCharRight(true);
+    } else {
+      setIsCharRight(false);
+      return;
+    }
+
+    if (value === currentWord) {
+      setCurrentIndex((prevIndex) => prevIndex + 1);
+      setUserInput("");
+      setMatchingChars(0);
+      setMatchingWords((prevMatchingWords) => prevMatchingWords + 1);
+    }
+    sendMessage();
+  };
+
+  const [isInputFocused, setIsInputFocused] = useState(false);
+
+  const handleInputFocus = () => {
+    setIsInputFocused(true);
+  };
+
+  const handleInputBlur = () => {
+    setIsInputFocused(false);
+  };
+
+  var isRoomValid = () => {
+    console.log(roomID);
+    if (roomID >= "1" && roomID <= "9") {
+      return true;
+    } else return false;
+  };
+
+  return (
+    <div className="flex justify-center items-start h-screen bg-neutral-200">
+      {isRoomValid() ? (
+        <div className="flex flex-wrap py-44 w-10/12">
+          {words.map((word, index) => {
+            const isCurrentWord = index === currentIndex;
+            const alreadyTyped = index < matchingWords;
+            return (
+              <div className="p-1 h-12 w-fit flex text-neutral-500" key={index}>
+                {word.split("").map((char, charIndex) => {
+                  const isMatching = charIndex < matchingChars;
+                  const isCaret = isCurrentWord && charIndex === matchingChars;
+                  const charClassName = `relative flex items-center text-xl font-medium border-l-2 ${
+                    isCurrentWord || alreadyTyped
+                      ? isMatching || alreadyTyped
+                        ? "text-neutral-900"
+                        : ""
+                      : ""
+                  } 
+                  ${
+                    isCaret
+                      ? isCharRight
+                        ? "border-yellow-500"
+                        : "border-red-500"
+                      : ""
+                  }
+                `;
+
+                  const hasOtherCaret =
+                    otherCaretPositions.find((pos) => {
+                      if (Array.isArray(pos) && pos.length === 2) {
+                        const [row, col] = pos;
+                        return row === index && col === charIndex;
+                      }
+                      return false;
+                    }) !== undefined;
+
+                  return (
+                    <div className={charClassName} key={charIndex}>
+                      {hasOtherCaret && (
+                        <div className="text-red-500 text-2xl absolute -top-3 -left-2 rotate-180">
+                          ^
+                        </div>
+                      )}
+
+                      {char}
+                    </div>
+                  );
+                })}
+              </div>
+            );
+          })}
+
+          <input
+            className={`${
+              isInputFocused ? "opacity-0" : "opacity-0"
+            } w-10/12 h-96 absolute top-44 left-46 text-lg text-center placeholder-neutral-900`}
+            type="text"
+            value={userInput}
+            onChange={handleChange}
+            onKeyDown={handleKeyDown}
+            onFocus={handleInputFocus}
+            onBlur={handleInputBlur}
+            placeholder="Click here to start typing"
+          />
+        </div>
+      ) : (
+        <div>This is not a valid room.</div>
+      )}
+    </div>
+  );
+};
+
+export default Room;
